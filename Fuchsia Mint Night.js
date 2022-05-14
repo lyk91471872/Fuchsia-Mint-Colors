64f1da0f31482186fcecbcc4b971a1e922f0dff1
Warren Luo <30332640+lyk91471872@users.noreply.github.com>
2022-05-14 08:32:24 -0400

    Add files via upload

---
+++ Fuchsia Mint Night.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#230025", "#e33d63", "#00a67b", "#244640", "#195c71", "#cf00e9", "#89c9ff", "#f39ff3", "#230025", "#e33d63", "#00a67b", "#244640", "#195c71", "#cf00e9", "#89c9ff", "#f39ff3"]);
+t.prefs_.set('foreground-color', "#6effc3");
+t.prefs_.set('background-color', "#00171c");
+t.prefs_.set('cursor-color', "#ffffff");
